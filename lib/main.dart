50a557be9ec504ee7204c9791269fb079f20c0a6
岡田 <ys.training221021@gmail.com>
2023-03-08 16:56:17 +0900

    3回目のコミット

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: AppBar(title: Text('こんにちは')),
     );
   }
 }
